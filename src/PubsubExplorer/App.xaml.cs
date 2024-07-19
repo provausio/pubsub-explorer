9e455dabe361a9d61e298db36d4509cc26b5386d
Jeremy Stafford <jestafford@petsmart.com>
2024-07-19 13:11:41 -0700

    wip: installer

--- src/PubsubExplorer/App.xaml.cs
+++ src/PubsubExplorer/App.xaml.cs
@@ -8,16 +8,4 @@ public partial class App : Application
 
         MainPage = new MainPage();
     }
-
-    protected override Window CreateWindow(IActivationState? activationState)
-    {
-        var window = base.CreateWindow(activationState);
-        window.Width = 1540;
-        window.Height = 1080;
-        window.MaximumWidth = double.PositiveInfinity;
-        window.MaximumHeight = 1080;
-        window.MinimumWidth = 900;
-        window.MinimumHeight = 1080;
-        return window;
-    }
 }
